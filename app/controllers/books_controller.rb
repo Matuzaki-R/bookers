7e4af2a521d1c7a616bcf8fd06d7fee619f332a8
”Matuzaki-R” <104716596+Matuzaki->
2022-06-04 13:11:33 +0000

    [Add]空投稿エラーメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,14 @@ class BooksController < ApplicationController
      redirect_to books_path(@book.id)
     else
      @books = Book.all
-     render :new
+     render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    
   end
 
   def show
@@ -28,12 +30,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
